5e9b45453646ad1c581b4ff1f896842bfa119d81
MrsRibeiroGabriela <ribeirogabriela.dev@gmail.com>
2023-02-02 04:28:46 -0300

    Folder: models

--- api/index.js
+++ api/index.js
@@ -2,10 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 
-import authRoute from "./routes/auth.js"
-import usersRoute from "./routes/users.js"
-import hotelsRoute from "./routes/hotels.js"
-import roomsRoute from "./routes/rooms.js"
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
 
 const app = express();
 dotenv.config();
@@ -22,16 +22,17 @@ const connect = async () => {
 
 mongoose.connection.on("disconnect", () => {
   console.log("mongoDB disconnected");
-})
+});
 
 //middlewares
-app.use("/api/v1/auth", authRoute)
-app.use("/api/v1/users", usersRoute)
-app.use("/api/v1/hotels", hotelsRoute)
-app.use("/api/v1/rooms", roomsRoute)
+app.use(express.json());
 
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", usersRoute);
+app.use("/api/v1/hotels", hotelsRoute);
+app.use("/api/v1/rooms", roomsRoute);
 
 app.listen(8800, () => {
-  connect()
+  connect();
   console.log("Connected to backend server!");
 });
