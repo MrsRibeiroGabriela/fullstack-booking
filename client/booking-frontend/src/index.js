f0b9ef498fe8e448d7398ac2f78378fd09c79d0e
MrsRibeiroGabriela <ribeirogabriela.dev@gmail.com>
2023-05-04 14:35:25 -0300

    [FEAT] Login page

--- client/booking-frontend/src/index.js
+++ client/booking-frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { SearchContext, SearchContextProvider } from "./context/SearchContext";
+import { SearchContextProvider } from "./context/SearchContext";
+import {AuthContextProvider} from "./context/AuthContext"
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SearchContextProvider>
-      <App />
-    </SearchContextProvider>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
